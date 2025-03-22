2b4c7d72b0c09fedc25cf8a73e7c12a11ccf82e7
Yuki Hayashi <mrchildreny@gmail.com>
2025-03-23 02:14:52 +0900

    開発環境でのプラットフォームセットアップのためのawait呼び出しに関するコメントを削除しました。

--- next.config.ts
+++ next.config.ts
@@ -23,8 +23,5 @@ const nextConfig: NextConfig = {
 export default nextConfig;
 
 if (process.env.NODE_ENV === 'development') {
-	// `await`ing the call is not necessary but it helps making sure that the setup has succeeded.
-	//  If you cannot use top level awaits you could use the following to avoid an unhandled rejection:
-	//  `setupDevPlatform().catch(e => console.error(e));`
 	await setupDevPlatform();
 }
